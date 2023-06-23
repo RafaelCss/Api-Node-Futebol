cfe38c002d10630297811ad03d4686613a139d6a
devspacerj <rf2014souza@gmail.com>
2023-06-22 22:31:47 -0300

    21-06 atualizando pacote

--- server.ts
+++ server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors"
 import routerLogin from "./src/Controllers/LoginController";
-import routerUsuarios from "./src/Controllers/UsuariosController";
-import routerGatos from "./src/Controllers/GatosController";
 import routerClientes from "./src/Controllers/ClientesController";
 
 
@@ -10,16 +8,14 @@ import routerClientes from "./src/Controllers/ClientesController";
 const app = express();
 
 
-app.use(cors({origin:"*"}))
+app.use(cors({ origin: "*" }))
 app.use(express.json());
-app.use("/api",routerLogin)
-app.use("/api",routerUsuarios)
-app.use("/api", routerGatos)
+app.use("/api", routerLogin)
 app.use("/api", routerClientes)
 
 const port = process.env.PORT_SERVER || 3001
 
-app.listen(port,() =>{
+app.listen(port, () => {
   console.log(`Você está conectado na porta ${port}`)
 })
 
