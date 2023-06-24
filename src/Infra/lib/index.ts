9a08b1ff05ce4b72082d5c13fdb60250132f73e9
devspacerj <rf2014souza@gmail.com>
2023-06-23 22:12:37 -0300

    23-06 atualizando pacote

--- src/Infra/lib/index.ts
+++ src/Infra/lib/index.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-export default class Prisma {
-  public static Prisma: PrismaClient;
+// export default class Prisma {
+//   public static Prisma: PrismaClient;
 
-  static getPrisma() {
-    //verify if prisma instance not exist
-    if (this.Prisma === null || !this.Prisma)
-      //create new one
-      this.Prisma = new PrismaClient()
-    return this.Prisma
-  }
-}
+//   static getPrisma() {
+//     //verify if prisma instance not exist
+//     if (this.Prisma === null || !this.Prisma)
+//       //create new one
+//       this.Prisma = new PrismaClient()
+//     return this.Prisma
+//   }
+// }
