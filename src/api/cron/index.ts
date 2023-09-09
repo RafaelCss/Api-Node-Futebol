b635476babed6c917911f81a4c29664014b443ea
RafaelCss <rf2014souza@gmail.com>
2023-09-09 07:02:47 -0300

    feature/09-09 correção no jobs de busca

--- src/api/cron/index.ts
+++ src/api/cron/index.ts
@@ -1,14 +1,7 @@
-import { salvarDadosTabelaNoDataBase } from "../../Infra/MongoDb";
-import buscarDadosTabelaCampeonato from "../../Services/ServicesExternos/jobTabela";
+import { salvarDadosTabelaNoDataBase } from '../../Infra/MongoDb';
+import buscarDadosTabelaCampeonato from '../../Services/ServicesExternos/jobTabela';
 
-
-export default async function handler(req: any, res: { status: (arg0: number) => { (): any; new(): any; end: { (arg0: string): void; new(): any; }; }; }) {
-    try { 
-      await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
-      console.log('Job executado com sucesso');
-      console.log(new Date());
-    } catch (error) {
-      console.error('Erro ao executar o job:', error);
-    }
-    res.status(200).end('Hello Cron!');
-  }
+export default async function handler() {
+  await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
+  console.log('Job executado com sucesso', new Date());
+}
