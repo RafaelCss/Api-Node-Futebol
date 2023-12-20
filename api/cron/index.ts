084fc50dd5130755ecb837be7fd589600ffb6798
RafaelCss <rf2014souza@gmail.com>
2023-12-19 21:17:47 -0300

    bugfix/19-12 Ajuste no envio das credenciais

--- api/cron/index.ts
+++ api/cron/index.ts
@@ -1,12 +1,11 @@
 import { salvarDadosTabelaNoDataBase } from '../../src/Infra/MongoDb';
 import buscarDadosTabelaCampeonato from '../../src/Services/ServicesExternos/jobTabela';
 import cron from 'node-cron';
-async function handler() {
+async function handler(req : Request , res :Response) {
   console.log('Job executado com sucesso', new Date());
 }
 
-cron.schedule('0 10,18  * * *', async () => {
-  handler() 
+cron.schedule('0 10 * * *', async () => {
   try { 
     await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
     console.log('Job executado com sucesso');
@@ -15,14 +14,14 @@ cron.schedule('0 10,18  * * *', async () => {
     console.error('Erro ao executar o job:', error);
   }
 });
-cron.schedule('* * * * *', async () => {
-  handler() 
-  try {
-    await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
-    console.log('Job executado com sucesso');
-    console.log(new Date());
-  } catch (error) {
-    console.error('Erro ao executar o job:', error);
+// cron.schedule('* * * * *', async () => {
+//   handler() 
+//   try {
+//     await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
+//     console.log('Job executado com sucesso');
+//     console.log(new Date());
+//   } catch (error) {
+//     console.error('Erro ao executar o job:', error);
 
-  }
-});
+//   }
+// });
