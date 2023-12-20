98aa43f1a4d3b04a003bdb99575b42c539d79f4a
RafaelCss <rf2014souza@gmail.com>
2023-12-19 21:13:03 -0300

    bugfix/19-12 Ajuste no envio das credenciais

--- api/cron/index.ts
+++ api/cron/index.ts
@@ -1,7 +1,28 @@
 import { salvarDadosTabelaNoDataBase } from '../../src/Infra/MongoDb';
 import buscarDadosTabelaCampeonato from '../../src/Services/ServicesExternos/jobTabela';
-
+import cron from 'node-cron';
 async function handler() {
-  await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
   console.log('Job executado com sucesso', new Date());
 }
+
+cron.schedule('0 10,18  * * *', async () => {
+  handler() 
+  try { 
+    await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
+    console.log('Job executado com sucesso');
+    console.log(new Date());
+  } catch (error) {
+    console.error('Erro ao executar o job:', error);
+  }
+});
+cron.schedule('* * * * *', async () => {
+  handler() 
+  try {
+    await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
+    console.log('Job executado com sucesso');
+    console.log(new Date());
+  } catch (error) {
+    console.error('Erro ao executar o job:', error);
+
+  }
+});
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,11 @@ import cors from "cors"
 import routerLogin from "./Controllers/LoginController";
 import routerUsuario from "./Controllers/UsuarioController";
 import tabelaCampeonato from "./Controllers/TabelaCampeonato";
-import cron from 'node-cron';
-import { salvarDadosTabelaNoDataBase } from "./Infra/MongoDb";
-import buscarDadosTabelaCampeonato from "./Services/ServicesExternos/jobTabela";
+
 
 const app = express();
 
-app.use(cors({ origin: "*" ,credentials:true, allowedHeaders:["content-type"]}))
+app.use(cors({ origin: "*"}))
 app.use(express.json());
 app.use("/api", routerLogin)
 app.use("/api", routerUsuario)
@@ -22,27 +20,6 @@ app.get("/", async (_req: Request, res: Response) => {
   res.send({ "msg": "você está conectado" })
 })
 
-
-
-// cron.schedule('0 10,18  * * *', async () => {
-//   try { 
-//     await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
-//     console.log('Job executado com sucesso');
-//     console.log(new Date());
-//   } catch (error) {
-//     console.error('Erro ao executar o job:', error);
-//   }
-// });
-// cron.schedule('* * * * *', async () => {
-//   try {
-//     await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
-//     console.log('Job executado com sucesso');
-//     console.log(new Date());
-//   } catch (error) {
-//     console.error('Erro ao executar o job:', error);
-
-//   }
-// });
 app.listen(port, () => {
   console.log(`Você está conectado na porta ${port}`)
 })
