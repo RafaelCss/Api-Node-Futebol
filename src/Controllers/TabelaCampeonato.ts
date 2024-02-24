8d7359e11c8ddc165cab7e3da9d6b972cd6ae4d2
RafaelCss <rf2014souza@gmail.com>
2024-02-24 14:42:13 -0300

    teste cron

--- src/Controllers/TabelaCampeonato.ts
+++ src/Controllers/TabelaCampeonato.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express'
 import servico from '../Aplicacao/Tabela/QueryTabela';;
 import IPaginacao from '../Domain/Interfaces/Paginacao';
 import { validarToken } from '../Services/Seguranca/ValidarToken';
+import { salvarDadosTabelaNoDataBase } from '../Infra/MongoDb';
+import buscarDadosTabelaCampeonato from '../Services/ServicesExternos/jobTabela';
 
 
 
@@ -10,8 +12,19 @@ const tabelaCampeonato = Router();
 tabelaCampeonato.get("/tabela",validarToken ,async (req: Request, res: Response) => {
     const comando = await servico.buscarTabela()
     res.send(comando)
-  })
+})
 
+
+tabelaCampeonato.get("/atualizar-tabela",async (req: Request, res: Response) => {
+  try { 
+    await salvarDadosTabelaNoDataBase(await buscarDadosTabelaCampeonato());
+    console.log('Job executado com sucesso');
+    console.log(new Date());
+  } catch (error) {
+    console.error('Erro ao executar o job:', error);
+  }
+  console.log('Job executado com sucesso', new Date());
+})
 
 export default tabelaCampeonato;
 
