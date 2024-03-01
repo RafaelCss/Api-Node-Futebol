47a7551f359f3fe90f067f9aee1f913cd4b7fa88
RafaelCss <rf2014souza@gmail.com>
2024-02-29 23:08:51 -0300

    feature/29-02 Inplementando senha cryptografada

--- src/Services/Seguranca/Senha/index.ts
+++ src/Services/Seguranca/Senha/index.ts
@@ -1,18 +1,46 @@
+import bcrypt from 'bcrypt';
+
 interface PropsEncryptSenha {
-  gerarSenha: (senha: string) => string;
+  gerarSenha: (senha: string) => Promise<string>;
+  checkUser: (senha: string, senhaHash: string) => Promise<boolean>;
 }
 
-class EncryptSenha implements PropsEncryptSenha {
+ class EncryptSenha implements PropsEncryptSenha {
   #senha: string;
-  constructor(senha: string) {
+  constructor() {
     this.#senha = '';
-    this.gerarSenha(senha);
   }
-  public gerarSenha(senha: string) {
-    const encryptSenha = this.encryptSenha(senha);
-    return this.#senha;
+  /**
+   * @description
+   * função que retorna senha criptografada
+   */
+  public  async gerarSenha(senha: string) {
+   const hashSenha =  await this.encryptSenha(senha);
+    return hashSenha;
   }
-  protected encryptSenha(senha: string) {
-    this.#senha = senha;
+  /**
+   *
+   * @param senha
+   * senha a ser criptografada
+   */
+  protected async encryptSenha(senha: string) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(senha, salt);
+   return this.#senha = hash;
+  }
+  /**
+   * @param senha
+   * senha vinda do front
+   * @param senhaHash
+   * hash da senha vinda do banco
+   */
+  public async checkUser(senha: string, senhaHash: string) {
+    const match = await bcrypt.compare(senha, senhaHash);
+    if (match) {
+      return true;
+    }
+    return false;
   }
 }
+
+export default EncryptSenha;
